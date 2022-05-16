1d4fa8b3770db7f08261b9ac22120787e2480fbd
Si Thu San <sithusan@aceplussolutions.com>
2022-05-16 17:17:23 +0630

    Test Some Basic Routing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ABCController } from './abc.controller';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Book } from './library/book.entity';
@@ -17,7 +18,7 @@ import { Book } from './library/book.entity';
       synchronize: true,
     }),
   ],
-  controllers: [AppController],
+  controllers: [AppController, ABCController],
   providers: [AppService],
 })
 export class AppModule {}
